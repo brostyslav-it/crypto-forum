197e7457d483d0902326975a67b5399d5e31f61b
brostyslav <bykhalrostyslav@gmail.com>
2024-12-25 20:49:16 +0000

    add laravel triggers

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,10 +6,41 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
     protected $guarded = [];
+
+    protected static function booted(): void
+    {
+        static::created(function ($post) {
+            DB::table('action_logs')->insert([
+                'action_type' => 'INSERT',
+                'table_name' => 'posts',
+                'action_description' => 'Inserted a new post with ID ' . $post->id . ' titled ' . $post->title,
+                'created_at' => now(),
+            ]);
+        });
+
+        static::updated(function ($post) {
+            DB::table('action_logs')->insert([
+                'action_type' => 'UPDATE',
+                'table_name' => 'posts',
+                'action_description' => 'Updated post with ID ' . $post->id . ' from ' . $post->getOriginal('title') . ' to ' . $post->title,
+                'created_at' => now(),
+            ]);
+        });
+
+        static::deleted(function ($post) {
+            DB::table('action_logs')->insert([
+                'action_type' => 'DELETE',
+                'table_name' => 'posts',
+                'action_description' => 'Deleted post with ID ' . $post->id . ' titled ' . $post->title,
+                'created_at' => now(),
+            ]);
+        });
+    }
 
     public function user(): BelongsTo
     {
---
+++ database/migrations/2024_12_25_204433_action_logs.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('action_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('action_type', 50);
+            $table->string('table_name', 50);
+            $table->text('action_description');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('action_logs');
+    }
+};
